0e0c99b9e395af965b2103e15db4d8f886a73ae1
myname21-meet <myname21@meet.mit.edu>
2019-10-31 21:03:47 -0400

    flask stuff

--- app.py
+++ app.py
@@ -5,11 +5,22 @@ app = Flask(__name__)
 app.secret_key = "MY_SUPER_SECRET_KEY"
 
 
-##### Code here ######
+@app.route('/')
+def homepage():
+	return render_template("home.html")
+
+@app.route('/store')
+def store():
+	return render_template("store.html")
+
+@app.route('/about')
+def about():
+	return render_template("about.html")
 
 
-
-#####################
+@app.route('/store/cart')
+def cart():
+	return render_template("cart.html")
 
 
 if __name__ == '__main__':
